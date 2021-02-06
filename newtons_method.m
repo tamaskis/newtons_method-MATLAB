fa693f7bc8b951921d6432de933919b38a12737d
tamaskis <tamas.a.kis@outlook.com>
2021-02-05 19:30:51 -0800

    Add files via upload

--- newtons_method.m
+++ newtons_method.m
@@ -24,15 +24,21 @@
 %   df is the derivative of f(x) (i.e. f'(x)), x0 is an initial guess of 
 %   the root, TOL is the tolerance, and imax is the maximum number of 
 %   iterations.
+% 
+%   root = newtons_method(__,'all') returns a vector, where the first
+%   element of this vector is the initial guess, all intermediate elements
+%   are the intermediate estimates of the root, and the last element is the
+%   converged root. This identifier 'all' may be appended to any of the
+%   syntaxes used above.
 %
 % See also fzero
 %
 % MATLAB Central File Exchange: https://www.mathworks.com/matlabcentral/fileexchange/85735-newton-s-method-newtons_method
-% GitHub: https://github.com/tamaskis/compressible_flow_relations-MATLAB
+% GitHub: https://github.com/tamaskis/newtons_method-MATLAB
 %
 % See "Newton's Method - MATLAB implementation" for additional
-% documentation. An example can be found in EXAMPLES_.m. Both of these
-% these files are included with the download.
+% documentation. Examples can be found in EXAMPLES.m. Both of these files
+% are included with the download.
 %
 % Copyright (c) 2021 Tamas Kis
 
@@ -43,47 +49,100 @@
 % INPUT: f - function handle for f(x)
 %        df - function handle for f'(x)
 %        x0 - initial guess for root
-%        TOL - tolerance (OPTIONAL)
-%        imax - maximum number of iterations (OPTIONAL)
+%        TOL - (OPTIONAL) tolerance
+%        imax - (OPTIONAL) maximum number of iterations
+%        output - if specified as 'all', function will returns all 
+%                 intermediate root estimates; otherwise, a faster 
+%                 algorithm is used to only return the converged root
 % OUTPUT: root - root of f(x)
-function root = newtons_method(f,df,x0,TOL,imax)
+%          --> if "output" is specified as 'all', then "root" will be a 
+%              vector, where the first element is the initial guess, the 
+%              last element is the converged root, and the other elements 
+%              are intermediate estimates of the root
+%          --> otherwise, "root" is a single number storing the converged
+%              root
+function root = newtons_method(f,df,x0,TOL,imax,output)
     
-    % sets default tolerance and maximum number of iterations depending on
-    % which inputs are specified by user
-    if nargin < 4
+    % sets default tolerance and maximum number of iterations if not
+    % specified by user
+    if (nargin < 4) || isempty(TOL)
         TOL = 1e-12;
+    end
+    if (nargin < 5) || isempty(imax)
         imax = 1e6;
-    elseif nargin == 4
-        imax = 1e6;
-    elseif (nargin == 5) && isempty(TOL)
-        TOL = 1e-12;       
+    end
+    
+    % decides which algorithm to use
+    if nargin < 6
+        return_all = false;
+    else
+        if strcmp(output,'all')
+            return_all = true;
+        else
+            return_all = false;
+        end
     end
     
     % initializes the error so the loop will be entered
     err = 2*TOL;
     
-    % preallocates x
-    x = zeros(imax,1);
+    % implements algorithm for Newton's method where all intermediate root
+    % estimates are also returned
+    if return_all
+        
+        % preallocates x
+        x = zeros(imax,1);
+
+        % inputs initial guess for root into x vector
+        x(1) = x0;
+
+        % Newton's method
+        i = 1;
+        while (i < imax) && (err > TOL)
+
+            % updates estimate of root
+            x(i+1) = x(i)-f(x(i))/df(x(i));
+
+            % calculates error
+            err = abs(x(i+1)-x(i));
+
+            % increments loop index
+            i = i+1;
+
+        end
     
-    % inputs initial guess for root into x vector
-    x(1) = x0;
+        % returns converged root along with intermediate root estimates
+        root = x(1:i);
 
-    % Newton's method
-    i = 1;
-    while (i < imax) && (err > TOL)
-
-        % updates estimate of root
-        x(i+1) = x(i)-f(x(i))/df(x(i));
+    % implements (faster) algorithm for Newton's method where only the
+    % converged root estimate is returned
+    else
         
-        % calculates error
-        err = abs(x(i+1)-x(i));
+        % sets root estimate at the first iteration of Newton's method as 
+        % the initial guess
+        x_old = x0;
 
-        % increments loop index
-        i = i+1;
-        
+        % Newton's method
+        i = 1;
+        while (i < imax) && (err > TOL)
+
+            % updates estimate of root
+            x_new = x_old-f(x_old)/df(x_old);
+
+            % calculates error
+            err = abs(x_new-x_old);
+
+            % stores updated root estimate for next iteration
+            x_old = x_new;
+
+            % increments loop index
+            i = i+1;
+
+        end
+
+        % returns root
+        root = x_new;
+      
     end
-    
-    % returns root
-    root = x(i);
       
 end
